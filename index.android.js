c6a81d56e83d8f708d9bf061bb800b1f0748b3e3
xclidongbo <xclidongbo@gmail.com>
2017-09-09 15:41:05 +0800

    无更改

--- index.android.js
+++ index.android.js
@@ -17,7 +17,7 @@ export default class ReactNativeDemo extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to RN
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
